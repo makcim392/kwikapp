02beed2d9f5534dbcfffc68dacc14f60ca64159e
makcim392 <sz.makcim@gmail.com>
2023-07-11 20:46:40 -0300

    Otros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ const App = () => {
     <h1>Welcome to React, {3 + 3}</h1>
     <p>{nombre}</p>
     <h2>2 + 2</h2>
+    
     <input type="text"></input>
+    <input type="password"></input>
     </>
   )
 }
