33ed75239885df38640c06aa82f5300a816a99b2
makcim392 <sz.makcim@gmail.com>
2023-07-11 20:59:12 -0300

    Agregar linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-const App = () => {
-
-  let nombre = 'Juan'
+function App() {
+  const nombre = 'Juan';
 
   return (
-<>
-    <h1>Welcome to React, {3 + 3}</h1>
-    <p>{nombre}</p>
-    <h2>2 + 2</h2>
-    
-    <input type="text"></input>
-    <input type="password"></input>
+    <>
+      <h1>
+        Welcome to React,
+        {3 + 3}
+      </h1>
+      <p>{nombre}</p>
+      <h2>2 + 2</h2>
+
+      <input type="text" />
+      <input type="password" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
