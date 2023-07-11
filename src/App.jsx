867edc1a9fc54d87b3341f433c85564add657d34
makcim392 <sz.makcim@gmail.com>
2023-07-11 20:43:04 -0300

    Clase 3

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+
+  let nombre = 'Juan'
+
+  return (
+<>
+    <h1>Welcome to React, {3 + 3}</h1>
+    <p>{nombre}</p>
+    <h2>2 + 2</h2>
+    <input type="text"></input>
+    </>
+  )
+}
+
+export default App
