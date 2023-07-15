ab4ae10bf38492329ce43c0ca3b4838f2bb2bb80
makcim392 <sz.makcim@gmail.com>
2023-07-15 18:10:28 -0300

    Fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Box } from '@chakra-ui/react';
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div>
-      <h1>
-        Welcome to Kwikapp
-      </h1>
       <Box bg="blue.500" color="red" p={4}>
         This is a Chakra UI Box component.
       </Box>
+      <NavBar />
     </div>
 
   );
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line react/function-component-definition
+const NavBar = () => (
+  <div>
+    <h1>
+      Welcome to Kwikapp
+    </h1>
+  </div>
+);
+
+export default NavBar;
