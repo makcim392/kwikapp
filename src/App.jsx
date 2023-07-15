bf78cd47d1977d56747474c3fe35781aa561e062
makcim392 <sz.makcim@gmail.com>
2023-07-15 17:24:41 -0300

    Clear App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
 function App() {
-  const nombre = 'Juan';
-
   return (
-    <>
-      <h1>
-        Welcome to React,
-        {3 + 3}
-      </h1>
-      <p>{nombre}</p>
-      <h2>2 + 2</h2>
-
-      <input type="text" />
-      <input type="password" />
-    </>
+    <h1>
+      Welcome to Kwikapp
+    </h1>
   );
 }
 
