a60a9aeb26a54db02f90ef7c67a6bd07c246e852
makcim392 <sz.makcim@gmail.com>
2023-07-15 17:18:41 -0300

    Clear unused configs and add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const NavBar = () => (
+  <div>
+    NavBar Component
+  </div>
+);
+
+NavBar.propTypes = {};
+
+NavBar.defaultProps = {};
+
+export default NavBar;
