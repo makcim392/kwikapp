219f1167bd332bdf60f4bb6ce3a7000b42d5590a
makcim392 <sz.makcim@gmail.com>
2023-07-15 17:49:54 -0300

    Establish yarn as default package manager
    Removed all things related to npm, added chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { Box } from '@chakra-ui/react';
+
 function App() {
   return (
-    <h1>
-      Welcome to Kwikapp
-    </h1>
+    <div>
+      <h1>
+        Welcome to Kwikapp
+      </h1>
+      <Box bg="blue.500" color="red" p={4}>
+        This is a Chakra UI Box component.
+      </Box>
+    </div>
+
   );
 }
 
