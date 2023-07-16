a0600b22d82da9d6e597975bd8bd1dfae4503a98
makcim392 <sz.makcim@gmail.com>
2023-07-16 20:03:12 -0300

    Update type of App function

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { Box, ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 
-function App() {
-  return (
-    <ChakraProvider>
-      <Box bg="blue.500" color="white" p={4}>
-        Kwikappa
-      </Box>
-      <NavBar />
-    </ChakraProvider>
+const App = () => (
+  <ChakraProvider>
+    <Box bg="blue.500" color="white" p={4}>
+      Kwikapp
+    </Box>
+    <NavBar />
+  </ChakraProvider>
 
-  );
-}
+);
 
 export default App;
