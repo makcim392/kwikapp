979120989c1d7b929debba18f37910f22b58f541
makcim392 <sz.makcim@gmail.com>
2023-07-16 00:11:45 -0300

    Add widget

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable func-names */
 module.exports = function ( api ) {
-    api.cache( true );
+
 
     var env = api.cache( () => process.env.NODE_ENV );
     var isProd = api.cache( () => process.env.NODE_ENV === "production" );
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/react-in-jsx-scope */
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
@@ -6,8 +5,8 @@ import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <ChakraProvider>
-      <Box bg="blue.500" color="red" p={4}>
-        This is a Chakra UI Box component.
+      <Box bg="blue.500" color="white" p={4}>
+        Kwikappa
       </Box>
       <NavBar />
     </ChakraProvider>
