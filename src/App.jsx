f5406ee469b145fbabbdf8fd975ba3852888f8e5
makcim392 <sz.makcim@gmail.com>
2023-08-04 19:49:37 -0300

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
 const App = () => (
-  <ChakraProvider>
-    <Box bg="blue.500" color="white" p={4}>
-      <NavBar />
-      <ItemListContainer />
-    </Box>
-  </ChakraProvider>
+  <BrowserRouter>
+    <ChakraProvider>
+      <Box bg="blue.500" color="white" p={4}>
+        <NavBar />
+        <ItemListContainer />
+      </Box>
+    </ChakraProvider>
+  </BrowserRouter>
 
 );
 
