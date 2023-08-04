91dc0cba1249ac92d758bda1fe1f7178834454a7
makcim392 <sz.makcim@gmail.com>
2023-08-04 20:01:12 -0300

    Add basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
@@ -9,8 +10,13 @@ const App = () => (
     <ChakraProvider>
       <Box bg="blue.500" color="white" p={4}>
         <NavBar />
-        <ItemListContainer />
       </Box>
+      <Routes>
+        <Route exact path="/" element={<ItemListContainer />} />
+        <Route exact path="/category/:id" element={<ItemListContainer />} />
+        <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+      </Routes>
+
     </ChakraProvider>
   </BrowserRouter>
 
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ItemDetailContainer = () => (
+  <div>
+    <h1>Item Detail Container</h1>
+  </div>
+);
+
+export default ItemDetailContainer;
