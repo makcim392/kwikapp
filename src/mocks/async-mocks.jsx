a70d8a0edbf0bf6578685238856720dc802327c0
makcim392 <sz.makcim@gmail.com>
2023-08-06 22:00:13 -0300

    Add async mocks

---
+++ src/mocks/async-mocks.jsx
@@ -0,0 +1,59 @@
+const products = [
+  {
+    id: 1,
+    title: 'Product 1',
+    description: 'Description 1',
+    price: 100,
+    pictureUrl: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    description: 'Description 2',
+    price: 200,
+    pictureUrl: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 3,
+    title: 'Product 3',
+    description: 'Description 3',
+    price: 300,
+    pictureUrl: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 4,
+    title: 'Product 4',
+    description: 'Description 4',
+    price: 400,
+    pictureUrl: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 5,
+    title: 'Product 5',
+    description: 'Description 5',
+    price: 500,
+    pictureUrl: 'https://via.placeholder.com/150',
+  },
+];
+
+const categories = [
+  { id: 'category1', name: 'Categoría 1' },
+  { id: 'category2', name: 'Categoría 2' },
+
+];
+
+export function getProducts() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 1000);
+  });
+}
+
+export function getCategories() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(categories);
+    }, 500);
+  });
+}
