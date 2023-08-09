440d074445f5a215bece49aada72f282e2c5663b
makcim392 <makcim@mkc.uy>
2023-08-08 22:38:28 -0300

    PreEntrega2_Silveira

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ItemList = ({ products }) => {
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
