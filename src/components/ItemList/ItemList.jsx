1f9a4ac36ed41d9576098b5136504ce9ad6b9de7
makcim392 <makcim@mkc.uy>
2023-08-12 19:04:19 -0300

    Improve ItemList display

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,20 +1,24 @@
+import { Box, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 const ItemList = ({ products }) => {
   return (
-    <div>
-      ItemList works!
-      Products:
-      {' '}
-      {products.length}
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </div>
-      ))}
-    </div>
+    <Box>
+      <VStack spacing={4} align="start">
+        {products.map((product) => (
+          <Box key={product.id} p={4} borderWidth="1px" borderRadius="md" boxShadow="md">
+            <Text fontSize="lg" fontWeight="semibold">
+              {product.title}
+            </Text>
+            <Text>{product.description}</Text>
+            <Text fontSize="lg" fontWeight="bold">
+              $
+              {product.price}
+            </Text>
+          </Box>
+        ))}
+      </VStack>
+    </Box>
   );
 };
 
