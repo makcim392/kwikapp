55c17721eb64cc2f53deaefcfcb26dc6297e836e
makcim392 <makcim@mkc.uy>
2023-08-13 16:17:47 -0300

    Fix possible memory leak based on linter recommendation

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,23 +1,40 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 
 export const CartContext = createContext();
 const CartContextProvider = ({ children }) => {
   const [cartList, setCartList] = useState([]);
-  const addToCart = (item, qty) => { // implementa la funcionalidad para agregar un producto al carrito
+  const addToCart = (item, qty) => {
+    const itemInCart = cartList.find((i) => i.id === item.id);
+    if (itemInCart) {
+      const updatedCart = cartList.map((i) => {
+        if (i.id === item.id) {
+          return { ...i, qty: i.qty + qty };
+        }
+        return i;
+      });
+      setCartList(updatedCart);
+    } else {
+      setCartList([...cartList, { ...item, qty }]);
+    }
   };
+
   const removeList = () => {
   };
+
   const deleteItem = (id) => {
     const filtered = cartList.filter((item) => item.id !== id);
     setCartList(filtered);
   };
+
+  const contextValue = useMemo(() => ({
+    cartList, addToCart, removeList, deleteItem,
+  }), [cartList]); // Only update contextValue when cartList changes
+
   return (
-    <CartContext.Provider value={{
-      cartList, addToCart, removeList, deleteItem,
-    }}
-    >
-      { children }
+    <CartContext.Provider value={contextValue}>
+      {children}
     </CartContext.Provider>
   );
 };
+
 export default CartContextProvider;
