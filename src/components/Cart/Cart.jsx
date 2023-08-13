8a9b2f0f02cb5f94ecf13b32dff21dd26deae221
makcim392 <makcim@mkc.uy>
2023-08-13 16:21:54 -0300

    Add Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartItemList from '../CartItemList/CartItemList';
+
+const Cart = () => {
+  return (
+    <CartItemList />
+  );
+};
+
+export default Cart;
---
+++ src/components/CartItemList/CartItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CartItemList = () => {
+  return (
+    <div>CartItemList</div>
+  );
+};
+
+export default CartItemList;
