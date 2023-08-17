edfb53dabdf5bf3b7a7325c3fbd1722d60c42edc
makcim392 <makcim@mkc.uy>
2023-08-17 20:25:33 -0300

    Show selected product in ItemDetail

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,41 +1,49 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
+import { fetchItemsFromFirebase } from '../../Redux/Item/ItemAction';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
-   const [item, setItem] = useState({});
-   const [isLoading, setIsLoading] = React.useState(true);
+  const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const { id } = useParams();
-
+  const dispatch = useDispatch();
   const itemsFromStore = useSelector((state) => state.items.items);
   console.log('itemsFromStore: ', itemsFromStore);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setIsLoading(true);
-  if (id) {
-    const filteredItem = itemsFromStore.find((p) => p.id === parseInt(id, 10)); console.log('filteredItem: ', filteredItem);
-    setItem(filteredItem);
-  } else {
-    setItem({});
-  }
-  setIsLoading(false);
-  }, [id, itemsFromStore]);
+    if (id) {
+      // Fetch items from Firebase and update Redux store
+      dispatch(fetchItemsFromFirebase())
+        .then(() => {
+          const filteredItem = itemsFromStore.find((p) => p.id === id);
+          setItem(filteredItem || {});
+          console.log('items map', itemsFromStore.map((p) => p.id));
+          console.log('filteredItem: ', filteredItem);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching items:', error);
+          setIsLoading(false);
+        });
+    } else {
+      setItem({});
+      setIsLoading(false);
+    }
+  }, [id, dispatch]);
 
-  console.log('product sent from ItemDetailContainer: ', item);
+  console.log('item sent from ItemDetailContainer: ', item);
 
   return (
     <>
       {isLoading ? (
-        <p>Loading...</p>
-    ) : (
-      <>
-        <ItemDetail
-          product={item}
-        />
-      </>
+        <div className="loading-spinner">Loading...</div>
+      ) : (
+        <ItemDetail product={item} />
       )}
     </>
   );
--- src/firebase/Document.jsx
+++ src/firebase/Document.jsx
@@ -1,6 +1,5 @@
 import {
   collection,
-  doc, getDoc,
   getDocs,
   getFirestore,
 } from 'firebase/firestore';
@@ -14,16 +13,16 @@ const Document = () => {
   useEffect(() => {
     const db = getFirestore();
 
-    const itemRef = doc(db, 'items', 'mjAb4KZRSoXpKLVDNtZ5');
-    getDoc(itemRef).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log('Document data:', snapshot.data());
-      } else {
-        console.log('No such document!');
-      }
-    }).catch((error) => {
-      console.log('Error getting document:', error);
-    });
+    // const itemRef = doc(db, 'items', 'mjAb4KZRSoXpKLVDNtZ5');
+    // getDoc(itemRef).then((snapshot) => {
+    //   if (snapshot.exists()) {
+    //     console.log('Document data:', snapshot.data());
+    //   } else {
+    //     console.log('No such document!');
+    //   }
+    // }).catch((error) => {
+    //   console.log('Error getting document:', error);
+    // });
 
     const itemsCollection = collection(db, 'items');
     getDocs(itemsCollection).then((snapshot) => {
