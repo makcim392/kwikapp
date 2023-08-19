e03a1c81dbdbac1e4c856e84fc7c5f09dda7e9fe
makcim392 <makcim@mkc.uy>
2023-08-19 16:20:50 -0300

    Replace useHistory with useNavigate()

--- src/components/OrderComponent/OrderComponent.jsx
+++ src/components/OrderComponent/OrderComponent.jsx
@@ -1,13 +1,13 @@
 import { Button } from '@chakra-ui/react';
 import React, { useContext } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom'; // Import useHistory
+import { useNavigate } from 'react-router-dom'; // Import useHistory
 import addOrder from '../../Redux/Order/OrderActions';
 import CartContext from '../../context/CartContext';
 
 const OrderComponent = () => {
   const dispatch = useDispatch();
-  const history = useHistory(); // Get the history object
+  const navigate = useNavigate(); // Get the history object
 
   const cartContext = useContext(CartContext);
   const { finishPurchase } = cartContext;
@@ -23,14 +23,14 @@ const OrderComponent = () => {
         { id: 'item1', title: 'Item 1', price: 10 },
         { id: 'item2', title: 'Item 2', price: 15 },
       ],
-      date: new Date(),
+      // date: new Date(),
       total: 25,
     };
     finishPurchase();
     dispatch(addOrder(order));
 
     // Programmatically navigate to the "/orderSummary" route
-    history.push('/orderSummary');
+    navigate('/orderSummary');
   };
 
   return (
